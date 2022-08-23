dc4e7e7e90e4f54679c5167bcc8633b9170b8916
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-08-23 05:41:34 +0200

    oppo6853: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: alps Model: oppo6853 Script version: 2.4.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_oppo6853.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_oppo6853-user \
+    omni_oppo6853-userdebug \
+    omni_oppo6853-eng
